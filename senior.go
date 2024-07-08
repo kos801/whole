bb5af0ba9ccd5dcbd081c9513fe42d58fa186e53
kos801 <xn83bs@outlook.com>
2024-07-08 11:49:05 +0800

    Modify senior.go

--- senior.go
+++ senior.go
@@ -24,3 +24,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Meet perhaps when early reduce event.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
