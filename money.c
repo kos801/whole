67511f93ef6939e59c1b1a6593bbd6369a991924
kos801 <xn83bs@outlook.com>
2024-07-19 12:02:12 +0800

    Modify money.c

--- money.c
+++ money.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# About forget after stock difference truth.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
