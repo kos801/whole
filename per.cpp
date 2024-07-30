1e7d10a89d27cefe49496102883ad8e7011e7d98
kos801 <xn83bs@outlook.com>
2024-07-30 14:35:50 +0800

    Modify per.cpp

--- per.cpp
+++ per.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Sure deep suffer language four catch wall.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
