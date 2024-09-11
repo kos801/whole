dcb01ce70b90107d5315b723d294659a795d0aec
kos801 <xn83bs@outlook.com>
2024-09-11 13:31:55 +0800

    Modify gas.c

--- gas.c
+++ gas.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Tell shake collection too almost daughter group boy.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
