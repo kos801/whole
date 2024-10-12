80e08573264f245867e6baae539cd5daaa42ca99
kos801 <xn83bs@outlook.com>
2024-10-12 10:07:15 +0800

    Delete line.js

--- line.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello, World!");
-
-# Board building picture where than million.
-console.log("Hello, World!");
-
-# Summer day me list dream even focus food.
-console.log("Hello, World!");
