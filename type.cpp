cfb13a065fc2eb74980c62f0865087a094558648
kos801 <xn83bs@outlook.com>
2024-10-14 10:31:35 +0800

    Modify type.cpp

--- type.cpp
+++ type.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Buy allow family key want.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
