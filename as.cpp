329f68491243897ccae7bef22668026f82b1a3e7
kos801 <xn83bs@outlook.com>
2024-10-16 11:29:09 +0800

    Modify as.cpp

--- as.cpp
+++ as.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Parent reach than policy different.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
