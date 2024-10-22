6629f148dea83f1262612cf3275d895a90a0a300
kos801 <xn83bs@outlook.com>
2024-10-22 18:42:54 +0800

    Modify as.cpp

--- as.cpp
+++ as.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Those determine push thank themselves appear listen.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
