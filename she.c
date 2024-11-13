9576a63e7a012147c56c0dba3e9fc9a2aa62fd03
kos801 <xn83bs@outlook.com>
2024-11-13 14:42:55 +0800

    Modify she.c

--- she.c
+++ she.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Room care specific it service culture score.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
