49993396a166ee2c35a33af4ea63d10932af21a8
kos801 <xn83bs@outlook.com>
2024-11-20 14:32:13 +0800

    Modify she.c

--- she.c
+++ she.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Response executive later green value notice.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
