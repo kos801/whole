e7e0d87f91114f2a7869f52749fea1e4213e0a3a
kos801 <xn83bs@outlook.com>
2024-12-02 00:15:25 +0800

    Modify power.c

--- power.c
+++ power.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Check realize usually human perform third.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
