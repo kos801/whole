09da41f3a00cebf97e7eef3b842c3840649523e3
kos801 <xn83bs@outlook.com>
2024-12-02 00:15:25 +0800

    Modify power.c

--- power.c
+++ power.c
@@ -17,3 +17,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Hot plant state public left suddenly consider.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
