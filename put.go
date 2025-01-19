17728f0ea5515d4d5aedac8e507999bb9d7c6717
kos801 <xn83bs@outlook.com>
2025-01-19 14:24:59 +0800

    Modify put.go

--- put.go
+++ put.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Already against trade local manage increase.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
